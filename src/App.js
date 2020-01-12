b967e87fb5309cb36addb67511a2cae87101a027
seanRong <seanRong@users.noreply.github.com>
2020-01-12 12:01:22 -0800

    sfaf

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {Form, Button} from 'react-bootstrap';
 import './App.css';
+import * as tf from '@tensorflow/tfjs';
 
 
 export default class App extends Component {
@@ -28,7 +29,17 @@ export default class App extends Component {
     this.setState({ [event.target.name]: event.target.value});
   }
 
-  
+  componentDidMount() {
+    this.myPredict();
+  }
+
+  async myPredict() {
+    // const model = await tf.loadLayersModel('C:/Users/Sean/polinp/ml/js/model.json');
+    const model = await tf.loadLayersModel({"format": "layers-model", "generatedBy": "keras v2.2.4-tf", "convertedBy": "TensorFlow.js Converter v1.4.0", "modelTopology": {"keras_version": "2.2.4-tf", "backend": "tensorflow", "model_config": {"class_name": "Sequential", "config": {"name": "sequential", "layers": [{"class_name": "Embedding", "config": {"name": "embedding", "trainable": true, "batch_input_shape": [null, null], "dtype": "float32", "input_dim": 44000, "output_dim": 32, "embeddings_initializer": {"class_name": "RandomUniform", "config": {"minval": -0.05, "maxval": 0.05, "seed": null}}, "embeddings_regularizer": null, "activity_regularizer": null, "embeddings_constraint": null, "mask_zero": false, "input_length": null}}, {"class_name": "GlobalAveragePooling1D", "config": {"name": "global_average_pooling1d", "trainable": true, "dtype": "float32", "data_format": "channels_last"}}, {"class_name": "Dense", "config": {"name": "dense", "trainable": true, "dtype": "float32", "units": 32, "activation": "relu", "use_bias": true, "kernel_initializer": {"class_name": "GlorotUniform", "config": {"seed": null}}, "bias_initializer": {"class_name": "Zeros", "config": {}}, "kernel_regularizer": null, "bias_regularizer": null, "activity_regularizer": null, "kernel_constraint": null, "bias_constraint": null}}, {"class_name": "Dense", "config": {"name": "dense_1", "trainable": true, "dtype": "float32", "units": 1, "activation": "sigmoid", "use_bias": true, "kernel_initializer": {"class_name": "GlorotUniform", "config": {"seed": null}}, "bias_initializer": {"class_name": "Zeros", "config": {}}, "kernel_regularizer": null, "bias_regularizer": null, "activity_regularizer": null, "kernel_constraint": null, "bias_constraint": null}}]}}, "training_config": {"loss": "binary_crossentropy", "metrics": ["accuracy"], "weighted_metrics": null, "sample_weight_mode": null, "loss_weights": null, "optimizer_config": {"class_name": "Adam", "config": {"name": "Adam", "learning_rate": 0.0010000000474974513, "decay": 0.0, "beta_1": 0.8999999761581421, "beta_2": 0.9990000128746033, "epsilon": 1e-07, "amsgrad": false}}}}, "weightsManifest": [{"paths": ["group1-shard1of2.bin", "group1-shard2of2.bin"], "weights": [{"name": "dense/kernel", "shape": [32, 32], "dtype": "float32"}, {"name": "dense/bias", "shape": [32], "dtype": "float32"}, {"name": "dense_1/kernel", "shape": [32, 1], "dtype": "float32"}, {"name": "dense_1/bias", "shape": [1], "dtype": "float32"}, {"name": "embedding/embeddings", "shape": [44000, 32], "dtype": "float32"}]}]});
+
+    let res = model.predict(tf.tensor1d(["Liberal"]));
+    console.log(res)
+  }
 
   scrapePage() {
     var element = document.getElementById("result");
@@ -86,7 +97,7 @@ export default class App extends Component {
     let request_params = {
           method : 'GET',
           hostname : host,
-          path : path + '?q=' + encodeURIComponent(search) + '&count=30',
+          path : path + '?q=' + encodeURIComponent(search) + '&count=50',
           headers : {
               'Ocp-Apim-Subscription-Key' : subscriptionKey,
           },
@@ -132,7 +143,7 @@ export default class App extends Component {
           <h3 className="">Your article:</h3>
 
           <div className="card col-lg-12 topic ">
-            <h3 className="mt-2 text-center">{this.state.userArticle}</h3>
+            <h3 className="mt-2 text-center"><a href={this.state.userArticle}>{this.state.userArticle}</a></h3>
           </div>
 
           <div className="card col-lg-12 bias neutral">
@@ -148,7 +159,7 @@ export default class App extends Component {
           <h3 className="mt-5">Our suggested reading:</h3>
 
           <div className="card col-lg-12 topic ">
-            <h3 className="mt-2 text-center">{this.state.links[1]}</h3>
+          <h3 className="mt-2 text-center"><a href={this.state.links[1]}>{this.state.links[1]}</a></h3>
           </div>
 
           <div className="card col-lg-12 bias neutral">
@@ -164,7 +175,7 @@ export default class App extends Component {
           <h3 className="mt-5">Our suggested reading:</h3>
 
           <div className="card col-lg-12 topic ">
-            <h3 className="mt-2 text-center">{this.state.links[2]}</h3>
+            <h3 className="mt-2 text-center"><a href={this.state.links[2]}>{this.state.links[2]}</a></h3>
           </div>
 
           <div className="card col-lg-12 bias neutral">
@@ -179,7 +190,7 @@ export default class App extends Component {
         <div className="col-lg-12 text-center">
         <h3 className="mt-5">Other links</h3>
         <div>{this.state.links.map(link => (
-            <li key={link}>{link}</li>
+            <li key={link}><a href={link}>{link}</a></li>
           ))}</div>
         </div>
 
